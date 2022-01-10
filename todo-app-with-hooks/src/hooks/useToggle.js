5fa4202c664fd4d68cb9228052b4d788640dd480
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-10 14:37:21 +0100

    created hook useToggle

---
+++ todo-app-with-hooks/src/hooks/useToggle.js
@@ -0,0 +1,12 @@
+import { usestate } from "react";
+
+function useToggle(initialVal = false) {
+
+    const [state, setState] = useState(initialVal);
+    const toggle = () => {
+        setState(!state);
+    }
+    return [state, toggle];
+}
+
+export default useToggle;
