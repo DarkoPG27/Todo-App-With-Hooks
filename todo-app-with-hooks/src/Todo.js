d68e84508270d556fbd5de49dfa643cec268e37f
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-01-10 13:36:52 +0100

    Added Edit and Delete icons

--- todo-app-with-hooks/src/Todo.js
+++ todo-app-with-hooks/src/Todo.js
@@ -1,11 +1,25 @@
-import { ListItem, ListItemText } from "@mui/material";
+import { ListItem, ListItemText, Checkbox, ListItemSecondaryAction, IconButton } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 function Todo({ task, completed }) {
     return (
         <ListItem>
-            <ListItemText>
+            <Checkbox tabIndex={-1} checked={completed} />
+            <ListItemText
+                style={{ textDecoration: completed ? "line-through" : "none" }}
+            >
                 {task}
             </ListItemText>
+            <ListItemSecondaryAction>
+                <IconButton aria-label="Edit">
+                    <EditIcon></EditIcon>
+                </IconButton>
+                <IconButton aria-label="Delete">
+                    <DeleteIcon></DeleteIcon>
+                </IconButton>
+
+            </ListItemSecondaryAction>
         </ListItem>
     );
 }
